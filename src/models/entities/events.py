f8e1b03bdf02fb29589bec2d175459aaac13800e
Clayton Rios <junioorrioos06@gmail.com>
2024-04-02 23:05:49 -0300

    Start

---
+++ src/models/entities/events.py
@@ -0,0 +1,11 @@
+from src.models.settings.base import Base
+from sqlalchemy import Column, String, Integer
+
+class Events(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    details = Column(String)
+    slug = Column(String, nulable=False)
+    maximum_attendees = Column(Integer)
