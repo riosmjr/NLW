c8b2035b58de8b9bc2bbc4b0f3ded173a50acfce
Clayton Rios <junioorrioos06@gmail.com>
2024-04-02 23:51:55 -0300

    Finish first class

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -7,5 +7,8 @@ class Events(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     details = Column(String)
-    slug = Column(String, nulable=False)
+    slug = Column(String, nullable=False)
     maximum_attendees = Column(Integer)
+
+    def __repr__(self):
+        return f"Events [id={self.id}, title={self.title}, details={self.details}, slug={self.slug}, maximum_attendees={self.maximum_attendees}]"
---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,24 @@
+import pytest
+from src.models.settings.connection import db_connection_handler
+from .events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro em banco de dados")
+def test_insert_event():
+    event = {
+        "uuid": "meu-uuid",
+        "title": "title",
+        "slug": "slug",
+        "maximum_attendees": 20
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+    print(response)
+
+def test_get_event_by_id():
+    event_id = "meu-uuid"
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+    print(response)
